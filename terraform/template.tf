3dc90c6927c3e0821838d11c9957e67ba3e8c3cd
debashis ghosh <debashisghosh@debumac.local>
2017-01-21 01:38:33 -0600

    terraform template update

--- terraform/template.tf
+++ terraform/template.tf
@@ -5,4 +5,28 @@ resource "aws_instance" "lab" {
     tags {
         Name = "lab"
     }
+   key_name = "debashis1982-new"
+    provisioner "file" {
+      source = "../target/lab-spring-boot-0.1.0.jar"
+      destination = "/tmp/lab-spring-boot-0.1.0.jar"
+      connection {
+            type = "ssh"
+            user = "ec2-user"
+            private_key = "${file("~/.ssh/debashis1982-new.pem")}"
+      }
+    }
+    provisioner "remote-exec" {
+        inline = [
+          "sudo yum -y install java-1.8.0",
+          "sudo yum -y remove java-1.7.0-openjdk",
+          "chmod 755 /tmp/lab-spring-boot-0.1.0.jar",
+          "sudo ln -s /tmp/lab-spring-boot-0.1.0.jar /etc/init.d/labjava",
+          "sudo service labjava start"
+        ]
+         connection {
+            type = "ssh"
+            user = "ec2-user"
+            private_key = "${file("~/.ssh/debashis1982-new.pem")}"
+         }
+    }
 }
