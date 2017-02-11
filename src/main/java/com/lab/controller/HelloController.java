c4eb17bd32944069d692c3d208ff9e72be5e6cdb
debashis1982 <debashis1982@gmail.com>
2017-02-11 00:13:22 -0600

    Update HelloController.java

--- src/main/java/com/lab/controller/HelloController.java
+++ src/main/java/com/lab/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
 
-        return "Heroku rocks again!!";
+        return "Heroku rocks again and again!!";
     }
 
 }
