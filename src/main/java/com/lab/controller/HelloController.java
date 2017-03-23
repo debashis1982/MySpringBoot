be5e93122f44dc815b541e1ca2c5dc67d6162522
a027780 <debashis.ghosh@target.com>
2017-03-23 16:09:16 -0500

    jsp

--- src/main/java/com/lab/controller/HelloController.java
+++ src/main/java/com/lab/controller/HelloController.java
@@ -7,13 +7,25 @@ package com.lab.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.lab.model.Location;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.lab.model.Location;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 @RestController
 public class HelloController {
-
+    @Autowired
+    private Location location;
     @RequestMapping("/")
     public String index() {
-
-        return "Heroku rocks again and again and again!!";
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+	logger.debug("inside index of hellocontroller");
+        return "Heroku rocks again and again and again from "+location.getCountry();
     }
 
 }
